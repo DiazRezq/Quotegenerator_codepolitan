2439a19933c1c47c13b3b8fbd84f5dab5fd5ae8d
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-02-14 16:56:53 +0700

    Quote generator ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+import {useEffect, useState} from "react";
+
+ export default function App() {
+const [quote, setQuotes] = useState("")
+
+  async function getQuote(){
+    const res = await fetch('https://dummyjson.com/quotes/random')
+    const data = await res.json()
+    setQuotes(`${data.quote} - ${data.author}`)
+  }
+
+   useEffect(() => {
+     getQuote()
+   }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App">
+      <h1>Quotes Generator</h1>
+      <p>{quote}</p>
+      <button onClick={getQuote}>Generate</button>
+    </div>
     </>
   )
 }
 
-export default App
